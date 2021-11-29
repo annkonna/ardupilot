31a3714052ed78e36cbe320f9ce5b4c446f20db8
spittaro17 <43007625+spittaro17@users.noreply.github.com>
2021-11-29 16:43:46 -0500

    Update RMM.h
    Updated to reflect Meta-Data inside region (Also has declarations for integration further into Ardu through repopulate_regions())

--- libraries/AP_RMM/RMM.h
+++ libraries/AP_RMM/RMM.h
@@ -1,10 +1,11 @@
 #pragma once 
+#include <AP_InertialSensor.h>
 
 class RMM {
-     void* pool;
-    int free_offset;
-    int pool_size;
-    bool top_level;
+    void* pool;
+    //int* pool_size;
+    //bool* top_level;
+    //int* free_offset;
 
     RMM(void* parent_pool, int parent_free_offset, int size_in_bytes);
 
@@ -18,4 +19,17 @@ public:
     RMM* create_nested_region(int size_in_bytes);
 
     void print_status();
+
+    void dump_region();
+
+    //This Function Would Be Ardu Specific
+    void repopulate_regions();
+
+private:
+    #define TOP_LEVEL 0
+    #define SIZE 0+sizeof(int)
+    #define OFFSET 0+sizeof(int)+sizeof(int)
+    #define FULL 0+sizeof(int)+sizeof(int)+sizeof(int)
+
+    void init_allocate(int size_in_bytes);
 };
