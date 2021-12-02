9f086c71fb97dd8cbdbcb331895dd7e60cadeebb
Ann Gina Konnayil <annkonna@buffalo.edu>
2021-12-02 17:17:35 -0500

    added methods MemUsed and MemFree with their metadata in the region

--- libraries/AP_RMM/RMM.h
+++ libraries/AP_RMM/RMM.h
@@ -1,13 +1,15 @@
 #pragma once 
-#include <AP_InertialSensor.h>
 
 class RMM {
     void* pool;
     //int* pool_size;
     //bool* top_level;
     //int* free_offset;
+    //int nested_count;
+    //RMM* parent;
 
-    RMM(void* parent_pool, int parent_free_offset, int size_in_bytes);
+    RMM(void* parent_pool, int parent_free_offset, int size_in_bytes, RMM* p);
+    void set_nested_count();
 
 public:
     RMM(int size_in_bytes = 10 * 1024 * 1024); //default is 10 MB
@@ -18,18 +20,23 @@ public:
 
     RMM* create_nested_region(int size_in_bytes);
 
+    int get_nested_count();
+
+    int MemFree();
+
+    int MemUsed();
+
     void print_status();
 
     void dump_region();
 
-    //This Function Would Be Ardu Specific
-    void repopulate_regions();
-
 private:
-    #define TOP_LEVEL 0
-    #define SIZE 0+sizeof(int)
-    #define OFFSET 0+sizeof(int)+sizeof(int)
-    #define FULL 0+sizeof(int)+sizeof(int)+sizeof(int)
+    #define RMM_TOP_LEVEL       (0)
+    #define RMM_SIZE            (0+sizeof(int))
+    #define RMM_OFFSET          (0+sizeof(int)+sizeof(int))
+    #define RMM_NESTED_COUNT    (RMM_OFFSET+sizeof(int))
+    #define RMM_PARENT_PTR      (RMM_NESTED_COUNT+sizeof(int))
+    #define RMM_FULL            (RMM_PARENT_PTR+sizeof(void*))
 
     void init_allocate(int size_in_bytes);
 };
